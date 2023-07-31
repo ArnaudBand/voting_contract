149571fc0feebd0086d5b1ce10fc8345f2f8bbff
ArnaudBand <bandonkeyea@gmail.com>
2023-07-31 12:59:42 +0300

    Create newProposal that add a new proposal

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,10 +2,21 @@
 pragma solidity ^0.8.19;
 
 contract Voting {
-      struct Proposal {
+    struct Proposal {
         address target;
         bytes data;
         uint yesCount;
         uint noCount;
+    }
+
+    Proposal[] public proposals;
+
+    function newProposal(address _target, bytes memory _data) external {
+        proposals.push(Proposal({
+            target: _target,
+            data: _data,
+            yesCount: 0,
+            noCount: 0
+        }));
     }
 }
