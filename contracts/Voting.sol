4b46daf947b1908be4102fa053c0a0e29dc5d840
ArnaudBand <bandonkeyea@gmail.com>
2023-07-31 20:11:51 +0300

    Modify the castVote function to allow voters to change their vote on a particular proposal.

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,27 +2,42 @@
 pragma solidity ^0.8.19;
 
 contract Voting {
+
+    enum VoteStates{ Yes, No, Absent }
     struct Proposal {
         address target;
         bytes data;
         uint yesCount;
         uint noCount;
+        mapping(address => VoteStates) votes;
     }
 
     Proposal[] public proposals;
 
     // Create a new proposal to call 'target' with 'data'
     function newProposal(address _target, bytes memory _data) external {
-        proposals.push(Proposal(_target, _data, 0, 0));
+        Proposal storage proposal = proposals.push();
+        proposal.target = _target;
+        proposal.data = _data;
     }
 
     // Vote on proposal #`_proposalId`, `_yes` for yes, `_no` for no
     function castVote(uint _proposalId, bool _yes) external {
         Proposal storage proposal = proposals[_proposalId];
+
+        if(proposal.votes[msg.sender] == VoteStates.Yes) {
+            proposal.yesCount--;
+        } else if(proposal.votes[msg.sender] == VoteStates.No) {
+            proposal.noCount--;
+        }
+
+        // Check if the proposal is still open
         if (_yes) {
             proposal.yesCount++;
         } else {
             proposal.noCount++;
         }
+
+        proposal.votes[msg.sender] = _yes ? VoteStates.Yes : VoteStates.No;
     }
 }
