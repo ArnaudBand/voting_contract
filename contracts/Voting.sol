2e24980d5c09a3ee7a7ec9fc47a333a51b220b71
ArnaudBand <bandonkeyea@gmail.com>
2023-07-31 13:09:45 +0300

    Add castVote to know the votes

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -11,7 +11,18 @@ contract Voting {
 
     Proposal[] public proposals;
 
+    // Create a new proposal to call 'target' with 'data'
     function newProposal(address _target, bytes memory _data) external {
         proposals.push(Proposal(_target, _data, 0, 0));
+    }
+
+    // Vote on proposal #`_proposalId`, `_yes` for yes, `_no` for no
+    function castVote(uint _proposalId, bool _yes) external {
+        Proposal storage proposal = proposals[_proposalId];
+        if (_yes) {
+            proposal.yesCount++;
+        } else {
+            proposal.noCount++;
+        }
     }
 }
